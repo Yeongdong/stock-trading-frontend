c12eed9cbac2b0e71944cc52a0e96fdcf525aaa5
Yeongdong <jyd37855@gmail.com>
2025-05-14 14:15:37 +0900

    refactor: add "use client"

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import { useEffect } from "react";
 import StockOrderForm from "@/components/features/stock/StockOrderForm";
 import { STORAGE_KEYS } from "@/constants/auth";
+import { ErrorProvider } from "@/contexts/ErrorContext";
 
 export default function DashboardPage() {
   useEffect(() => {
@@ -17,9 +20,11 @@ export default function DashboardPage() {
   }, []);
 
   return (
-    <div className="container">
-      <h1>대시보드</h1>
-      <StockOrderForm />
-    </div>
+    <ErrorProvider>
+      <div className="container">
+        <h1>대시보드</h1>
+        <StockOrderForm />
+      </div>
+    </ErrorProvider>
   );
 }
